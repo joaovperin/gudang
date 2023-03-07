67dc458b7b49a19e1eed7146607b66553665ce77
Jonas Rafael Duarte dos Santos <32223850+jonas-duarte@users.noreply.github.com>
2023-03-07 19:09:37 -0300

    simplify tests

--- test/log.test.ts
+++ test/log.test.ts
@@ -1,61 +1,28 @@
 import * as gudangjs from "../src";
+import { LogLevel } from "../src";
 
 const Log = gudangjs.Log as any;
 
+const cases = [
+    [LogLevel.TRACE, 5],
+    [LogLevel.DEBUG, 4],
+    [LogLevel.INFO, 3],
+    [LogLevel.WARN, 2],
+    [LogLevel.ERROR, 1],
+];
+
 describe("Log", () => {
-    it("trace", () => {
-        Log.log = jest.fn();
-        gudangjs.Log.level = gudangjs.LogLevel.TRACE;
-        Log.trace("trace");
-        Log.debug("debug");
-        Log.info("info");
-        Log.warn("warn");
-        Log.error("error");
-        expect(Log.log).toHaveBeenCalledTimes(5);
-    });
-
-    it("debug", () => {
-        Log.log = jest.fn();
-        gudangjs.Log.level = gudangjs.LogLevel.DEBUG;
-        Log.trace("trace");
-        Log.debug("debug");
-        Log.info("info");
-        Log.warn("warn");
-        Log.error("error");
-        expect(Log.log).toHaveBeenCalledTimes(4);
-    });
-
-    it("info", () => {
-        Log.log = jest.fn();
-        gudangjs.Log.level = gudangjs.LogLevel.INFO;
-        Log.trace("trace");
-        Log.debug("debug");
-        Log.info("info");
-        Log.warn("warn");
-        Log.error("error");
-        expect(Log.log).toHaveBeenCalledTimes(3);
-    });
-
-    it("warn", () => {
-        Log.log = jest.fn();
-        gudangjs.Log.level = gudangjs.LogLevel.WARN;
-        Log.trace("trace");
-        Log.debug("debug");
-        Log.info("info");
-        Log.warn("warn");
-        Log.error("error");
-        expect(Log.log).toHaveBeenCalledTimes(2);
-
-    });
-
-    it("error", () => {
-        Log.log = jest.fn();
-        gudangjs.Log.level = gudangjs.LogLevel.ERROR;
-        Log.trace("trace");
-        Log.debug("debug");
-        Log.info("info");
-        Log.warn("warn");
-        Log.error("error");
-        expect(Log.log).toHaveBeenCalledTimes(1);
-    });
+    test.each(cases)(
+        "when level is %s, log should be called %i times",
+        ((level: string, expected: number) => {
+            Log.log = jest.fn();
+            gudangjs.Log.level = level as LogLevel;
+            Log.trace("trace");
+            Log.debug("debug");
+            Log.info("info");
+            Log.warn("warn");
+            Log.error("error");
+            expect(Log.log).toHaveBeenCalledTimes(expected);
+        }) as any
+    );
 });
